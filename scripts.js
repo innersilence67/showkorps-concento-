27443bf89d2ab09d8c5e1b06bb8811799247b19c
doris <innersilence67@gmail.com>
2019-07-25 16:45:22 +0200

    test for rich text editor for "over ons"

---
+++ scripts.js
@@ -0,0 +1,6 @@
+$('#summernote').summernote({
+    height: 1000, // set editor height
+    minHeight: null, // set minimum height of editor
+    maxHeight: null, // set maximum height of editor
+    focus: true // set focus to editable area after initializing summernote
+});
